e192923dc7ba9eb05a203c98aa2c26a4bdf87d98
Indrata <69934537+myindrata@users.noreply.github.com>
2021-08-21 16:03:53 +0700

    Add files via upload

--- butMenu/butMenu.ino
+++ butMenu/butMenu.ino
@@ -11,7 +11,7 @@
 
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin[4] = {4,5,6,7};    // the number of the pushbutton pin
-const int ledPin = 13;      // the number of the LED pin
+const int ledPin = 2;      // the number of the LED pin
 
 // Variables will change:
 int ledState = LOW;         // the current state of the output pin
@@ -22,7 +22,11 @@ int lastButtonState[4] = {HIGH,HIGH,HIGH,HIGH};   // the previous reading from t
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastDebounceTime[4];  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-int count[4]={0,10,20,30};
+int count[4]={0,1,1,1};
+String menu, menuStr;
+int wthres;
+
+
 void setup() {
   Serial.begin(115200);
   for(int x;x<4;x++)pinMode(buttonPin[x], INPUT_PULLUP);
@@ -34,37 +38,132 @@ void setup() {
 }
 
 void loop() {
-for(int x=0;x<4;x++)count[x]=debounce(x);
+for(int x=0;x<4;x++)menuStr=debounce(x);
 
 }
 
-int debounce(int x){
-  int reading = digitalRead(buttonPin[x]);
+String debounce(int x){
+  int reading = digitalRead(buttonPin[x]);  
   if (reading != lastButtonState[x]) {
     lastDebounceTime[x] = millis();
-    
-  }
-
+  } 
   if ((millis() - lastDebounceTime[x]) > debounceDelay) {
-    if (reading != buttonState[x]) {
+      if (reading != buttonState[x]) {
       buttonState[x] = reading;
       if (buttonState[x] == LOW) {
         ledState = !ledState;
-        count[x]++;
-        but_max();
-        Serial.println(count[x]);
+         if (x==0){count[x]++;count[1]=0;count[2]=0;count[3]=0;}
+         if (x==1)count[x]++; 
+         if (x==2)count[1]--; 
+         but_max();
+         menu=String(count[0])+String(count[1]);     
+         /*
+         Serial.print(menu);
+         Serial.print(" - ");
+         Serial.print(count[0]);
+         Serial.print(" - ");
+         Serial.println(count[1]);
+         */
+         //OK
+         if(x==0)main_menu(count[0]);
+         if(x==1 || x == 2)sub_menu(menu.toInt());
+         if(x==3)menu_ok(menu.toInt());    
         }
     }
   }
 
   digitalWrite(ledPin, ledState);
   lastButtonState[x] = reading;
-  return count[x];
+  return menu;
 }
 
 void but_max(){
-  if (count[0]>4)count[0]=1;
-  if (count[0]==1 && count[1]>12)count[1]=11;
-  if (count[0]==2 && (count[1]>24 || count[1]<=20))count[1]=21;
-  if (count[0]==3 && (count[1]>1000 || count[1]<=0) )count[1]=31;
+   if (count[0]>3)count[0]=1;
+   if (count[0]==1 && count[1]>2 ) count[1]=1; //BT Menu ON/OFF
+   if (count[0]==1 && count[1]<0 ) count[1]=2; //BT Menu ON/OFF
+   
+   if (count[0]==2 && count[1]>3) count[1]=1; // WF Menu ON/OF/Clr 
+   if (count[0]==2 && count[1]<0) count[1]=3; // WF Menu ON/OF/Clr 
+
+   if (count[0]==3 && wthres>100) wthres=0; // Threshold menu
+   if (count[0]==3 && wthres<0) wthres=100;
+}
+
+void menu_ok(int var){
+  switch (var){
+    case 11:
+      Serial.println("BT ON OK");
+      break;
+    case 12:
+      Serial.println("BT OFF OK");
+      break;
+    case 21:
+      Serial.println("WF ON OK");
+      break;
+    case 22:
+      Serial.println("WF OFF OK");
+      break;
+    case 23:
+      Serial.println("CLR WF OK");
+      break;
+    case 30:
+      Serial.print("Threshold OK ");
+      Serial.print(wthres);
+      break;
+  }
+}
+
+void main_menu(int var){
+  switch (var){
+    case 1:
+      Serial.println("1. Set BT");
+      break;
+    case 2:
+      Serial.println("2. Set WIFI");
+      break;
+    case 3:
+      Serial.println("3. Set Threshold");
+      set_threshold();
+      break;
+  }
+}
+
+void sub_menu(int var){
+  switch (var){
+    case 11:
+      Serial.println("BT ON");
+      break;
+    case 12:
+      Serial.println("BT OFF");
+      break;
+    case 21:
+      Serial.println("WF ON");
+      break;
+    case 22:
+      Serial.println("WF OFF");
+      break;
+    case 23:
+      Serial.println("CLR WF");
+      break;
+  }
+}
+
+int set_threshold(){
+  while(count[0]==3){
+  int reading1 = digitalRead(buttonPin[1]);  
+  int reading2 = digitalRead(buttonPin[2]);  
+  if (reading1==0)wthres++;
+  if (reading2==0)wthres--;
+  but_max();
+  delay(100);
+   Serial.print(menu);
+   Serial.print(" - ");
+   Serial.print(count[0]);
+   Serial.print(" - ");
+   Serial.print(count[1]);
+   Serial.print(" - ");
+   Serial.println(wthres);
+  debounce(0);
+  debounce(3);
+  }
 }
